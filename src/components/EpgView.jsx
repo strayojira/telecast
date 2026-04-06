6e5f505f17b9414ff295821047e7baa09d34ab3a
ojira <diaz.jairomassimo@gmail.com>
2026-04-06 13:33:34 +0800

    Update EpgView.jsx

--- src/components/EpgView.jsx
+++ src/components/EpgView.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../lib/supabase";
 
 const DAY_LABELS = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
