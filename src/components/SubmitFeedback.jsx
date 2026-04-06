345124fbcdf1b077675413919d4b66cc5a867f31
ojira <diaz.jairomassimo@gmail.com>
2026-04-06 13:33:43 +0800

    Update SubmitFeedback.jsx

--- src/components/SubmitFeedback.jsx
+++ src/components/SubmitFeedback.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from "../lib/supabase";
 
 const DAY_PRESETS = [
   { label: "Daily", value: "Daily" },
